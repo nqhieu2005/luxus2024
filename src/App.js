7cf6a128af5159e2beea00887619f2e8d465d750
nqhieu2005 <hieunguyen2005q@gmail.com>
2024-10-24 21:58:47 +0700

    done login admin again

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import CompanyInfo from './components/contact';
 import Housing from './components/housing';
 import CommercialList from './components/commercial';
 import OfficeList from './components/office';
-import LoginPage from './admin/login';
+import Login from './admin/login';
 import ProductDetail from './components/housingDetails';
 import CommercialDetail from './components/commerrcialDetails';
 import OfficeDetail from './components/officeDetails';
-import Login from './admin/login';
 import AdminHome from './admin/adminPage';
 import AddProduct from './admin/addProduct';
 import NotFound from './admin/notfound';
@@ -22,8 +21,10 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    const loggedIn = sessionStorage.getItem('isLoggedIn') === 'true';
-    setIsAuthenticated(loggedIn);
+    const loggedIn = sessionStorage.getItem('isLoggedIn');
+    if (loggedIn === 'true') {
+      setIsAuthenticated(true);
+    }
   }, []);
 
   return (
@@ -31,18 +32,20 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LuxusInterior />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<CompanyInfo />} />
-          <Route path='/housing' element={<Housing />} />
-          <Route path='/commercial' element={<CommercialList />} />
-          <Route path='/office' element={<OfficeList />} />
-          <Route path='/admin' element={<LoginPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<CompanyInfo />} />
+          <Route path="/housing" element={<Housing />} />
+          <Route path="/commercial" element={<CommercialList />} />
+          <Route path="/office" element={<OfficeList />} />
+          <Route 
+            path="/admin" 
+            element={<Login setIsAuthenticated={setIsAuthenticated} />} 
+          />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/commercial/:id" element={<CommercialDetail />} />
           <Route path="/office/:id" element={<OfficeDetail />} />
 
           {/* Bảo vệ các trang admin */}
-          <Route path="/admin" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
           <Route 
             path="/admin/home" 
             element={isAuthenticated ? <AdminHome /> : <Navigate to="/admin" replace />} 
